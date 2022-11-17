eb5fd28832e3b72996a50f844dfd79d6ff511b22
Seb <sebcyde@hotmail.com>
2022-11-17 22:01:28 +0000

    navigation bug fixes

--- z/src/Components/Navbar/MainNavbar.tsx
+++ z/src/Components/Navbar/MainNavbar.tsx
@@ -3,6 +3,8 @@ import Logo from '../../assets/ZLogo.png';
 import { Button, Container, Nav, Navbar, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { UpdateMangaID } from '../../Store/Slices/MangaSlice';
+import { Update } from '../../Store/Slices/AnimeSlice';
 
 type Props = {};
 
@@ -14,10 +16,16 @@ function MainNavbar({}: Props) {
 	const StoreID = useSelector((state: any) => state.IDState);
 	const StoreMangaID = useSelector((state: any) => state.MangaIDState);
 	const [User, setUser] = useState<string>('Sebastian');
+	const dispatch = useDispatch();
 
 	const PullID = () => {
 		console.log('Anime StoreID:', StoreID);
 		console.log('Manga StoreMangaID:', StoreMangaID);
+	};
+
+	const ResetAll = () => {
+		dispatch(UpdateMangaID(0));
+		dispatch(Update(0));
 	};
 
 	return (
@@ -39,25 +47,55 @@ function MainNavbar({}: Props) {
 			</Container>
 			<Navbar.Collapse id="responsive-navbar-nav">
 				<Nav className="me-auto">
-					<Nav.Link eventKey="1" as={Link} to="/" style={NavLinkStyle}>
+					<Nav.Link
+						eventKey="1"
+						as={Link}
+						to="/"
+						style={NavLinkStyle}
+						onClick={ResetAll}
+					>
 						Homepage
 					</Nav.Link>
 
-					<Nav.Link eventKey="2" as={Link} to="anime" style={NavLinkStyle}>
+					<Nav.Link
+						eventKey="2"
+						as={Link}
+						to="anime"
+						style={NavLinkStyle}
+						onClick={ResetAll}
+					>
 						Anime
 					</Nav.Link>
-					<Nav.Link eventKey="3" as={Link} to="manga" style={NavLinkStyle}>
+					<Nav.Link
+						eventKey="3"
+						as={Link}
+						to="manga"
+						style={NavLinkStyle}
+						onClick={ResetAll}
+					>
 						Manga
 					</Nav.Link>
-					<Nav.Link eventKey="4" as={Link} to="mylists" style={NavLinkStyle}>
+					<Nav.Link
+						eventKey="4"
+						as={Link}
+						to="mylists"
+						style={NavLinkStyle}
+						onClick={ResetAll}
+					>
 						My Lists
 					</Nav.Link>
-					<Nav.Link eventKey="5" as={Link} to="settings" style={NavLinkStyle}>
+					<Nav.Link
+						eventKey="5"
+						as={Link}
+						to="settings"
+						style={NavLinkStyle}
+						onClick={ResetAll}
+					>
 						Settings
 					</Nav.Link>
-					<Navbar.Text style={NavLinkStyle}>
+					{/* <Navbar.Text style={NavLinkStyle}>
 						Signed in as: <a href="#login">{User}</a>
-					</Navbar.Text>
+					</Navbar.Text> */}
 				</Nav>
 				<Button onClick={PullID}>Pull Store ID</Button>
 			</Navbar.Collapse>
