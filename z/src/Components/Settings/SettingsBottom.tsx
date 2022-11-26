6c0a3e3671188ae3c755dfabc08b091d4cd2b211
Seb <sebcyde@hotmail.com>
2022-11-26 22:51:47 +0000

    settings page styling and dynamic user data

--- z/src/Components/Settings/SettingsBottom.tsx
+++ z/src/Components/Settings/SettingsBottom.tsx
@@ -18,9 +18,39 @@ function SettingsBottom() {
 
 	return (
 		<div className="SettingsBottomSection">
-			// Add user details read only here
-			<h2>There's Nothing Here Right Now!</h2>
-			<p>We're still working on this bit...</p>
+			<div className="SettingsPageRow">
+				<p>UserName</p>
+				<p>{user?.displayName ? user.displayName : 'Not Set'}</p>
+			</div>
+			<div className="SettingsPageRow">
+				<p>Name</p>
+				<p>To Add To DB?</p>
+			</div>
+			<div className="SettingsPageRow">
+				<p>Email</p>
+				<p>{user?.email}</p>
+			</div>
+			<div className="SettingsPageRow">
+				<p>Phone Number</p>
+				<p>{user?.phoneNumber ? user.phoneNumber : 'Not Set'}</p>
+			</div>
+			<div className="SettingsPageRow">
+				<p>Verified Email</p>
+				<p>{user?.emailVerified ? 'Verified' : 'Not Verified'}</p>
+			</div>
+			<div className="SettingsPageRow">
+				<p>Account Creation</p>
+				<p>{user?.metadata.creationTime}</p>
+			</div>
+			<div className="SettingsPageRow">
+				<p>Last Sign In</p>
+				<p>{user?.metadata.lastSignInTime}</p>
+			</div>
+			<div className="SettingsPageRow">
+				<p>User ID</p>
+				<p>{user?.uid}</p>
+			</div>
+
 			<Button onClick={LogOut}>Log Out</Button>
 		</div>
 	);
