24bb8933b6b6c35c01aa7d69aa9785b7327220b3
Sebastian <sebcyde@hotmail.com>
2022-12-08 19:12:57 +0000

    had to remove AOS from Anime Banners - caused non displaying bugs

--- z/src/Pages/Homepage/Homepage.tsx
+++ z/src/Pages/Homepage/Homepage.tsx
@@ -3,52 +3,44 @@ import WelcomeBanner from '../../Components/Homepage/WelcomeBanner';
 import AnimeListBanner from '../../Components/Anime/AnimeListBanner';
 import TopPoster from '../../Components/Homepage/TopPoster';
 import FavouritesCarousel from '../../Components/Homepage/FavouritesCarousel';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 type Props = {};
 
 function Homepage({}: Props) {
 	const [FaveCarousel, setFaveCarousel] = useState<any>();
-	const [Banner0, setBanner0] = useState<any>();
-	const [Banner1, setBanner1] = useState<any>();
-	const [Banner2, setBanner2] = useState<any>();
 	const [TopPoster0, setTopPoster0] = useState<any>();
+	const [Banner0, setBanner0] = useState<any>();
 
 	useEffect(() => {
 		setTimeout(() => {
 			setBanner0(
-				<AnimeListBanner
-					URL="https://api.jikan.moe/v4/seasons/upcoming"
-					Title="Upcoming Anime"
-				/>
-			);
-			setBanner1(
-				<AnimeListBanner
-					URL="https://api.jikan.moe/v4/top/manga"
-					Title="Top Manga"
-				/>
-			);
-			setBanner2(
 				<AnimeListBanner
 					URL="https://api.jikan.moe/v4/top/anime"
 					Title="Top Anime"
 				/>
 			);
 			setFaveCarousel(<FavouritesCarousel />);
-		}, 1000);
-		setTimeout(() => {
 			setTopPoster0(
 				<TopPoster URL="https://api.jikan.moe/v4/seasons/upcoming" />
 			);
-		}, 2000);
+		}, 1000);
 	}, []);
 
 	return (
 		<div className="page" style={{ overflowY: 'scroll' }}>
 			<WelcomeBanner />
 			<TopPoster URL="https://api.jikan.moe/v4/seasons/now" />
+			<AnimeListBanner
+				URL="https://api.jikan.moe/v4/seasons/upcoming"
+				Title="Upcoming Anime"
+			/>
+			<AnimeListBanner
+				URL="https://api.jikan.moe/v4/top/manga"
+				Title="Top Manga"
+			/>
 			{Banner0}
-			{Banner1}
-			{Banner2}
 			{FaveCarousel}
 			{TopPoster0}
 		</div>
