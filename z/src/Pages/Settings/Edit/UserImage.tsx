d2dbf8bbd9268d163b7852f2352ec425df6847c3
Seb <sebcyde@hotmail.com>
2022-12-11 15:49:37 +0000

    initial image selection screen build

--- z/src/Pages/Settings/Edit/UserImage.tsx
+++ z/src/Pages/Settings/Edit/UserImage.tsx
@@ -1,9 +1,109 @@
-import React from 'react';
+import { getAuth } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { db } from '../../../config/Firebase';
+import LoadingScreen from '../../LoadingScreen';
+import Image1 from '../../../assets/PFP/boy1.jpg';
+import Image2 from '../../../assets/PFP/boy2.jpg';
+import Image3 from '../../../assets/PFP/boy3.jpg';
+import Image4 from '../../../assets/PFP/boy5.jpg';
+import Image5 from '../../../assets/PFP/boy6.jpg';
+import Image6 from '../../../assets/PFP/Boy7.jpeg';
+import Image7 from '../../../assets/PFP/funnyboy1.jpeg';
+import Image8 from '../../../assets/PFP/funnygirl.jpeg';
+import Image9 from '../../../assets/PFP/funnygirl2.jpeg';
+import Image10 from '../../../assets/PFP/girl1.png';
+import Image11 from '../../../assets/PFP/girl2.png';
+import Image12 from '../../../assets/PFP/girl3.jpg';
+import Image13 from '../../../assets/PFP/Girl3.png';
+import Image14 from '../../../assets/PFP/girl4.jpg';
+import Image15 from '../../../assets/PFP/Girl5.jpg';
+import Image16 from '../../../assets/PFP/Girl6.jpg';
+import Image17 from '../../../assets/PFP/Girl7.jpg';
+import Image18 from '../../../assets/PFP/mask1.png';
+import { FaCheck } from 'react-icons/fa';
+import { ImageUpload } from '../../../Functions/ImageControl';
+import { Navigate, useNavigate } from 'react-router-dom';
 
-type Props = {};
+function UserImage() {
+	const [Loading, setLoading] = useState<boolean>(true);
+	const [UserDetails, setUserDetails] = useState<any>();
+	const navigate = useNavigate();
+	const auth = getAuth();
+	const user = auth.currentUser;
+	const images: string[] = [
+		Image1,
+		Image2,
+		Image3,
+		Image4,
+		Image5,
+		Image6,
+		Image7,
+		Image8,
+		Image9,
+		Image10,
+		Image11,
+		Image12,
+		Image13,
+		Image14,
+		Image15,
+		Image16,
+		Image17,
+		Image18,
+	];
 
-function UserImage({}: Props) {
-	return <div>UserImage</div>;
+	const PullData = async () => {
+		// Retrieve user details from DB
+		const docRef = doc(db, `Users/${user!.uid}`);
+		const docSnap = await getDoc(docRef);
+		if (docSnap.exists()) {
+			console.log(docSnap.data());
+			setUserDetails(docSnap.data());
+		} else {
+			console.log('Failed to retrieve user details');
+		}
+	};
+
+	const ChangePicture = async (URL: string) => {
+		setLoading(true);
+		await ImageUpload(user, URL);
+		navigate('/settings');
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		PullData().then(() => setLoading(false));
+	}, []);
+
+	return (
+		<div className="NewUserImageContainer">
+			{Loading ? (
+				<LoadingScreen />
+			) : (
+				<>
+					<div className="TopSection">
+						<Button>Cancel</Button>
+						<span>
+							<img src={UserDetails.DisplayPicture} />
+							<p>{UserDetails.Username}</p>
+						</span>
+						<Button>Save</Button>
+					</div>
+					<div className="ImagesContainer">
+						{images.map((Image: string) => {
+							return (
+								<div>
+									<img src={Image} onClick={() => ChangePicture(Image)} />
+									{UserDetails.DisplayPicture === Image ? <FaCheck /> : ''}
+								</div>
+							);
+						})}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default UserImage;
