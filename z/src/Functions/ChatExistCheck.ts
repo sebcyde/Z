e925ba55e63ca4b1dddc4076bf56a7d54c5e0f81
Sebastian <sebcyde@hotmail.com>
2022-12-27 12:42:46 +0000

    prep for v1.7.0 depoly

--- z/src/Functions/ChatExistCheck.ts
+++
@@ -1,8 +0,0 @@
-export const ChatAlreadyExists = async () => {
-	return ChatValue?.docs.filter((doc) => {
-		return (
-			doc.data().users[0] == user?.uid &&
-			doc.data().users[1] == QUserDetails.UID
-		);
-	});
-};
