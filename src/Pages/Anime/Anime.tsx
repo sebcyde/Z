eb55401390b742dedebe7db75e282dbeb6379552
Sebastian <sebcyde@hotmail.com>
2023-06-02 16:57:21 +0100

    cleanup

--- src/Pages/Anime/Anime.tsx
+++ src/Pages/Anime/Anime.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { useState } from 'react';
 import Jikan from '../../Components/Anime/Jikan';
-import AnimeDetails from './AnimeDetails';
-import LoadingScreen from '../LoadingScreen';
-import { useNavigate } from 'react-router-dom';
-import { Update } from '../../Store/Slices/AnimeSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import axios from 'axios';
 import '../../Styles/AnimeStyles.scss';
-import Container from 'react-bootstrap/Container';
-import TopPoster from '../../Components/Homepage/TopPoster';
 import SearchNavComponent from '../../Components/Search/SearchNavComponent';
 
 type Props = {};
 
 function Anime({}: Props) {
-	const StoreID = useSelector((state: any) => state.IDState);
-	const [AnimeName, setAnimeName] = useState<string>('');
-	const [Page, setPage] = useState(<Jikan />);
-	const dispatch = useDispatch();
+	// const StoreID = useSelector((state: any) => state.IDState);
+	// const [AnimeName, setAnimeName] = useState<string>('');
+	// const [Page, setPage] = useState(<Jikan />);
 
 	return (
 		<div className="page" style={{ overflowY: 'scroll' }}>
